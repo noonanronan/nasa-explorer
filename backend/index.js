791d7ba9d12d18cb26dbb87c0b95bbe23565df65
noonanronan <ronannoonan322@yahoo.com>
2025-06-18 14:52:22 +0100

    Added Mars Rover photos section with image cards

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,14 @@
-const express = require('express'); // The web Framework for creating routes
+const express = require('express'); // The web Framework
 const axios = require('axios'); // For HTTP Requests 
-const cors = require('cors'); // For frontend and backend comms (on diff port)
+const cors = require('cors'); // Frontend and backend communication (on diff port)
 require('dotenv').config(); // To use enviroment variables form the env file
 
 const app = express();
-const PORT = process.emvPORT || 5000;
+const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors()); // Enable CORS for all routes
 
+// Route to get Astronomy Picture of the Day from NASA API
 app.get('/api/apod', async (req, res) => {
     try{
         const response = await axios.get(
@@ -19,6 +20,18 @@ app.get('/api/apod', async (req, res) => {
     }
 });
 
+app.get('/api/mars', async (req, res) => {
+    try{
+        const response = await axios.get(
+            `https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=${process.env.NASA_API_KEY}` 
+        );
+        res.json(response.data.photos);
+    } catch (error) {
+        res.status(500).json({ message: 'Error fetching data for Mars rover'})
+    }
+});
+
+// Start the backend server
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
 });
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 
 function App() {
   const [apod, setApod] = useState(null); // Store the NASA APOD data (Astronomy Picture of the Day)
+  const [marsPhotos, setMarsPhotos] = useState([]); // Store Mars pictures
 
 
   useEffect(() => {
@@ -14,8 +15,19 @@ function App() {
       .catch(error => {
         console.error('Error fetching apod:', error);
       });
-  }, []);
-  
+  }, []); // Empty array to	run the effect only once
+
+  useEffect(() => {
+    // Fetch Mrs pic data from backend on component load
+    axios.get('http://localhost:5000/api/mars')
+      .then(response => {
+        setMarsPhotos(response.data);
+      })
+      .catch(error => {
+        console.error('Error fetching apod:', error);
+      });
+  }, []); // Empty array to	run the effect only once
+
   return (
     <div style={{ padding: '2rem', fontFamily: 'Arial' }}>
       <h1>NASA Astronomy Picture of the Day</h1>
@@ -27,6 +39,25 @@ function App() {
           <p>{apod.explanation}</p>
         </div>
       )}
+
+      <h1>Mars Rover Photos (Sol 1000)</h1>
+
+      <div style={{ display: 'flex', flexWrap: 'wrap', gap: '1rem' }}>
+        {marsPhotos.slice(0, 10).map((photo) => (
+          <div key={photo.id} style={{ width: '300px' }}>
+            <img
+              src={photo.img_src}
+              alt={`Taken by ${photo.rover.name} with ${photo.camera.full_name}`}
+              style={{ width: '100%' }}
+            />
+            <p>
+              <strong>Rover:</strong> {photo.rover.name}<br />
+              <strong>Camera:</strong> {photo.camera.full_name}<br />
+              <strong>Earth Date:</strong> {photo.earth_date}
+            </p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
