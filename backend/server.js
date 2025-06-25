5038192695450ecf831ebcce84df0edcc54ae404
Ronan Noonan <114731040+noonanronan@users.noreply.github.com>
2025-06-25 13:30:35 +0100

    Update server.js to have 5000 port

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,5 @@
 const app = require('./app');
-const PORT = process.env.PORT || 10000; // Port that matches render
-
+const PORT = process.env.PORT || 5000; 
 // Start the backend server
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
